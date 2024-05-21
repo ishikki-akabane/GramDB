06e3db748d2930af51711a6ccf1b552d4e9b78d7
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-21 22:04:58 +0545

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 api_id = 14681826
 api_hash = 'add59ab14dbbccf3c92c65ca4477f2fa'
 
-token = '6191819669:AAH-BrQM5FiaBSdZBOo8TaXv
+token = '6191819669:AAH-BrQM5FiaBSdZBOo8TaXv90GyO1rmljE'
 
 app = Client("bluebot", api_id=api_id, api_hash=api_hash, bot_token=token)
 
