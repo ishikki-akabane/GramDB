0459118bba863dc36b483e5323c3c59b1068fbf5
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-21 22:01:39 +0545

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+
+
+api_id = 14681826
+api_hash = 'add59ab14dbbccf3c92c65ca4477f2fa'
+
+token = '6191819669:AAH-BrQM5FiaBSdZBOo8TaXv
+
+app = Client("bluebot", api_id=api_id, api_hash=api_hash, bot_token=token)
+
+
+@app.on_message(filters.command("ffilter"))
+async def filter_message(client, message):
+    filter_msg = message.text.split(None, 1)[1]
+    save_filter()
+    await message.reply_text("filter set successfully!!")
+
+
+
+app.run()
