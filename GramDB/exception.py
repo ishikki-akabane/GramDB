18e47a6e77ec2cc752c9b853c1f51f7603a2febc
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-23 21:58:18 +0545

    Create exception.py

---
+++ GramDB/exception.py
@@ -0,0 +1,14 @@
+class GramDBError(Exception):
+    pass
+
+class ConnectionError(GramDBError):
+    pass
+
+class NotFoundError(GramDBError):
+    pass
+
+class DuplicateTableError(GramDBError):
+    pass
+
+class ValidationError(GramDBError):
+    pass
