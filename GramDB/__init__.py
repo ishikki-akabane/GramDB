0fb642ef39a9c9b3f8d759b406c11b6c1b05a6b8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-23 21:37:10 +0545

    Update __init__.py

--- GramDB/__init__.py
+++ GramDB/__init__.py
@@ -1,8 +1,30 @@
-import requests
+import aiohttp
 
 
 class GramDB:
     def __init__(self, db_url: str):
         self.db_url = db_url
+        self.session = aiohttp.ClientSession()
+
+    async def create(self, table_name: str):
+        pass
+
+    async def delete(self, table_name: str):
+        pass
+
+    async def insert_data(self, table_name: str):
+        pass
+
+    async def update_data(self, table_name: str):
+        pass
+
+    async def delete_data(self, table_name: str):
+        pass
+
+    async def get_data(self, table_name: str):
+        pass
+
+    async def close(self):
+        await self.session.close()
   
 
