caedb4c3c6506d4765838d9ca4ff0694a5652930
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-28 15:40:34 +0545

    Update method.py

--- GramDB/method.py
+++ GramDB/method.py
@@ -12,3 +12,21 @@ async def fetch_func(session, base_url, token, data_id):
         result = await response.json()
     else:
         result = False
+
+
+async def insert_func(session, base_url, token, data):
+    url = base_url + "/insert"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data": data
+    }
+
+    response = await session.post(url, headers=headers, json=payload)
+    if response.status == 200:
+        result = await response.json()
+        return True, result
+    else:
+        return False, response.status
