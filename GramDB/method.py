94157d93e05424b8df1cbba3d6b54761beb626d7
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-28 15:56:26 +0545

    Update method.py

--- GramDB/method.py
+++ GramDB/method.py
@@ -32,3 +32,38 @@ async def insert_func(session, base_url, token, data):
         return True, result
     else:
         return False, response.status
+
+
+async def delete_func(session, base_url, token, data_id):
+    url = base_url + "/delete"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_id
+    }
+    response = await session.post(url, headers=headers, json=payload)
+    if response.status == 200:
+        result = await response.json()
+        return True, result
+    else:
+        return False, response.status
+
+
+async def update_func(session, base_url, token, data_id, data):
+    url = base_url + "/update"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    payload = {
+        "data_id": data_id,
+        "data": data
+    }
+    response = await session.post(url, headers=headers, json=payload)
+    if response.status == 200:
+        result = await response.json()
+        return True, result
+    else:
+        return False, response.status
