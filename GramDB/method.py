b3fbd1c0a815f26e0c4cac04ae6f849b99f73dca
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-28 13:39:44 +0545

    Update method.py

--- GramDB/method.py
+++ GramDB/method.py
@@ -1,1 +1,14 @@
 # methods
+
+async def fetch_func(session, base_url, token, data_id):
+    url = base_url + "/fetch"
+    params = {
+        "token": token,
+        "data_id": data_id
+    }
+
+    response = await session.get(url, params=params)
+    if response.status == 200:
+        result = await response.json()
+    else:
+        result = False
