a6f506272cfe5d0bc75e648736c7fc461cac4c65
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-30 23:25:13 +0545

    Update test.py

--- test.py
+++ test.py
@@ -40,7 +40,10 @@ db = GramDB("https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb")
 async def aa():
   a = await db.fetch("bio_table", {'bio': "I'm cat"})
   print(a)
-  
+  b = await db.fetch_all()
+  for i in b:
+    print("\n", i)
+    
 asyncio.run(aa())
 db.close()
 print("ending...")
