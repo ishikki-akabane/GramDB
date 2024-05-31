74653eea26eb8b2d44153a1e4f420bf2aa2a73d8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-05-31 22:44:12 +0545

    Update test.py

--- test.py
+++ test.py
@@ -47,7 +47,7 @@ async def aa():
     for k, l in j.items():
       print(l)
 
-  await db.create("info_table", ("name", "class", "section"))
+  await db.create("test_table", ("name", "class", "section"))
   print("hmm")
   d = await db.fetch_all()
   for m, n in d.items():
