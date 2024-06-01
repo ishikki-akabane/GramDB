b9fdaa8c4aded4e618806f8a95323811ba536baf
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-01 14:49:41 +0545

    Update test.py

--- test.py
+++ test.py
@@ -38,18 +38,7 @@ sample_efficitiantdb = {
 db = GramDB("https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb")
 
 async def aa():
-  #a = await db.fetch("bio_table", {'bio': "I'm cat"})
-  #print(a)
-
-  b = await db.fetch_all()
-  for i, j in b.items():
-    print("\n", i)
-    for k, l in j.items():
-      print(l)
-
-  await db.insert("bio_table", {"bio": "I'm horse"}, _m_id="76")
   await db.create("info_table", ("name", "class"))
-  await db.insert("info_table", {"name": "Rohan", "class": 7, }, _m_id="78")
   print("hmm")
   d = await db.fetch_all()
   for m, n in d.items():
