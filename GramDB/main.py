b88091b51852ced1154b407d435863d28ac96084
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-01 14:56:58 +0545

    Update main.py

--- GramDB/main.py
+++ GramDB/main.py
@@ -53,7 +53,7 @@ class GramDB:
     async def create(self, table_name: str, schema: tuple):
         sample_record = {field: "test" for field in schema}
         sample_record['_id'] = "sample1928"
-        result, mdata = await insert_func(self.session, self.url, self.token, str(sample_record))
+        result, mdata = await insert_func(self.session, self.url, self.token, sample_record)
         if result:
             _m_id = mdata["data_id"]
             sample_record['_m_id'] = _m_id
