5d84d45dfe7f9f68c8eb7c5d2fd3e91723a45dd8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-03 08:14:27 +0545

    Update main.py

--- GramDB/main.py
+++ GramDB/main.py
@@ -92,16 +92,13 @@ class GramDB:
 
     async def background_update(self, table_name, update_query, _m_id):
         await asyncio.sleep(8)
+        update_query["_table_"] = table_name
         record = await self.db.fetch(table_name, update_query)
-        print("test record: ", record[0])
         result, mdata = await update_func(self.session, self.url, self.token, _m_id, record[0])
-        print("file inserted")
         
     async def update(self, table_name: str, query: dict, update_query: dict):
         _m_id = await self.db.update(table_name, query, update_query)
-        print("added data : ", _m_id)
         await asyncio.create_task(self.background_update(table_name, update_query, _m_id))
-        print("running")
         return
 
     async def delete_table(self, table_name: str):
