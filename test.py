a0a58707283144310fc02625eedc43e78555caf8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-10 09:31:59 +0545

    Update test.py

--- test.py
+++ test.py
@@ -53,6 +53,12 @@ async def aa_test():
   a = await db.fetch_all()
   print(json.dumps(a, indent=4))    
   print("------------------------")
+
+
+  d = await db.update("eri_data",  {"_id": 8292003004}, {"name": "ishikki_akabane", "level": 9999})
+  a = await db.fetch_all()
+  print(json.dumps(a, indent=4))    
+  print("------------------------")
   
 
   print(await db.fetch("eri_data",  {"_id": 8292003004}))
