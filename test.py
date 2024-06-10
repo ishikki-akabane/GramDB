52e40d90d53665e0207043325acffccec5289929
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-10 09:26:04 +0545

    Update test.py

--- test.py
+++ test.py
@@ -33,10 +33,30 @@ async def aa_test():
   db = GramDB("https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb")
   
   a = await db.fetch_all()
-  print(json.dumps(a, indent=2))    
+  print(json.dumps(a, indent=4))    
   print("------------------------")
+  
+
+  b = await db.create("eri_data", ("_id", "name", "level"))
+  a = await db.fetch_all()
+  print(json.dumps(a, indent=4))    
+  print("------------------------")
+  
+
+  c = await db.insert("eri_data", {"_id": 8292003004, "name": "ishikki", "level": 999})
+  a = await db.fetch_all()
+  print(json.dumps(a, indent=4))    
+  print("------------------------")
+
+
+  d = await db.update("eri_data",  {"_id": 8292003004}, {"name": "ishikki_akabane", "level": 9999})
+  a = await db.fetch_all()
+  print(json.dumps(a, indent=4))    
+  print("------------------------")
+  
+
+  print(await db.fetch("eri_data",  {"_id": 8292003004}))
   db.close()
-    
 asyncio.run(aa_test())
 
 print("ending...")
