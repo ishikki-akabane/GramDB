607f6eb7f55b576a3140d991a7e876274ce24bac
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-06-14 10:43:37 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from GramDB import GramDB
 
 
 api_id = 14681826
@@ -9,6 +10,7 @@ token = '6191819669:AAH-BrQM5FiaBSdZBOo8TaXv90GyO1rmljE'
 app = Client("bluebot", api_id=api_id, api_hash=api_hash, bot_token=token)
 
 
+db = GramDB("https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb")
 #----------------------------------------------
 
 
@@ -23,8 +25,11 @@ async def setbio_message(client, message):
 @app.on_message(filters.command("bio"))
 async def bio_watcher(client, message):
     user_id = message.from_user.id
-    bio_msg = await get_bio(unique_client_id, user_id, "bio_table")
-    await message.reply_text(bio_msg)
+    bio_msg = await db.fetch("blue_db", {"_id": user_id})
+    if bio_msg:
+        await message.reply_text(f"Your bio: `{bio_msg}`")
+    else:
+        await message.reply_text("You haven't set any bio yet!!")
 
 
 @app.on_message(filters.command("allbio"))
