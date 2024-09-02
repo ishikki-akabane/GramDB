af433555df5db6134ebc92f4a4dfd7ba07f9980b
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-02 21:10:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -95,8 +95,8 @@ db = DATABASE("https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb
 
 async def boot():
     #await db.create("userss", ("_id", "upload", "batch"))
-    await db.create("files", ("_id", "message_id"))
-    #await db.create("batchhh", ("_id", "channel_id", "message_id"))
+    #await db.create("files", ("_id", "message_id"))
+    await db.create("batchhh", ("_id", "channel_id", "message_id"))
     await asyncio.sleep(10)
 
 asyncio.run(boot())
