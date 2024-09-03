968537bbac15ca7a81c72f3191d3e89a307461a3
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-03 16:37:45 +0530

    Update method.py

--- GramDB/method.py
+++ GramDB/method.py
@@ -37,7 +37,7 @@ async def insert_func(session, base_url, token, data, table_name):
             result = await response.json()
             return True, result
         else:
-            print("Gramdb Error >>>>", await response.json)
+            print("Gramdb Error >>>>", response.status, "\n", response.content)
             return False, response.json()
 
 
