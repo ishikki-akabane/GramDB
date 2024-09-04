1a910c4488c681840f25d9f69d3021c81271a5d1
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-04 11:27:37 +0530

    Update db_thread.py

--- GramDB/db_thread.py
+++ GramDB/db_thread.py
@@ -1,19 +1,31 @@
 import threading
 import asyncio
+import time
 
-class GramDBThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self.loop = asyncio.new_event_loop()
-        self.running = True
 
-    def run(self):
-        asyncio.set_event_loop(self.loop)
-        self.loop.run_forever()
+class GramDBThread:
+    def __init__(self, gram_db):
+        self.gram_db = gram_db
+        self._stop_event = threading.Event()
+        self._thread = threading.Thread(target=self._run)
+        self._thread.daemon = True  # Set as daemon so it stops when main thread stops
+
+    def start(self):
+        self._thread.start()
 
     def stop(self):
-        self.running = False
-        self.loop.stop()
+        self._stop_event.set()
+        self._thread.join()
 
-    def submit(self, coro):
-        return asyncio.run_coroutine_threadsafe(coro, self.loop)
+    def _run(self):
+        while not self._stop_event.is_set():
+            time.sleep(1)
+
+    def perform_background_task(self, table_name, _m_id):
+        try:
+            result, old_data = async_extract_func(self.gram_db.url, self.gram_db.token)
+            old_data[table_name] = [_m_id]
+            async with aiohttp.ClientSession() as newsession:
+                result2, response = await git_func(newsession, self.gram_db.url, self.gram_db.token, old_data)
+        except Exception as e:
+            raise GramDBError(f"Error in background create: {e}")
