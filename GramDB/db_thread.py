e9537c08931112243b5bb1a413e6f3aed5f0e97d
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-05 19:47:06 +0530

    Update db_thread.py

--- GramDB/db_thread.py
+++ GramDB/db_thread.py
@@ -4,7 +4,7 @@ import asyncio
 class GramDBAsync:
     def __init__(self):
         self.loop = asyncio.new_event_loop()
-        self.thread = threading.Thread(target=self.run_event_loop)
+        self.thread = threading.Thread(target=self.run_event_loop, name="GramDBAsync")
         self.thread.start()
 
     def run_event_loop(self):
