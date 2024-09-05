27de4c71532510ee4f57648b36d54d50cb7c0503
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-05 16:59:05 +0530

    Update db_thread.py

--- GramDB/db_thread.py
+++ GramDB/db_thread.py
@@ -127,7 +127,7 @@ class GramDBTaskRunner:
 
 
 
-class GramDBAsync:
+class GramDBAsyncccc:
     def __init__(self):
         self.loop = asyncio.new_event_loop()
         self.thread = threading.Thread(target=self.run_loop)
@@ -159,3 +159,26 @@ class TaskHandler:
 
     def create_task(self, coroutine: Callable[[], Any]):
         return self.loop.run_in_executor(None, self.loop.run_until_complete, coroutine)
+
+
+
+class GramDBAsync:
+    def __init__(self):
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self.run_event_loop)
+        self.thread.start()
+
+    def run_event_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    def close_func(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+
+    def close(self):
+        self.close_func()
+        self.thread.join()
+
+    def run_async(self, coroutine):
+        return asyncio.run_coroutine_threadsafe(coroutine, self.loop).result()
+
