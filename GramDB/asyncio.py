6d0bcbed86e8e29a66a1925ed878e1b7b7bbf748
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-06 14:19:23 +0530

    Update asyncio.py

--- GramDB/asyncio.py
+++ GramDB/asyncio.py
@@ -21,3 +21,14 @@ class GramDBAsync:
     def run_async(self, coroutine):
         return asyncio.run_coroutine_threadsafe(coroutine, self.loop).result()
 
+    def create_task(self, coroutine):
+        """
+        Create a task to run the given coroutine.
+        
+        This method is similar to asyncio.create_task but works within the context of this class.
+        
+        :param coroutine: The coroutine to be executed.
+        :return: The created task.
+        """
+        return self.loop.call_soon_threadsafe(self.loop.create_task, coroutine).result()
+
