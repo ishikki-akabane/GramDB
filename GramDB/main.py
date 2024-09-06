fe4dd204786a628e0a3e8ff1a2b5dc352984698d
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-06 09:15:39 +0530

    Update main.py

--- GramDB/main.py
+++ GramDB/main.py
@@ -11,17 +11,7 @@ import threading
 logger = logging.getLogger('GramDB')
 logger.setLevel(logging.DEBUG)
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)
-
-always_handler = logging.StreamHandler()
-always_handler.setLevel(logging.INFO)
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-always_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-logger.addHandler(always_handler)
 
 
 
